346b8cf5633b7160fa939ebb8f19039c1767b3d0
butnomingzi <admin@butnomingzi.com>
2023-04-22 17:33:55 +0800

    change UI

--- src/main/java/com/example/weblogicexp/HelloController.java
+++ src/main/java/com/example/weblogicexp/HelloController.java
@@ -28,12 +28,22 @@ public class HelloController {
     @FXML
     private TextArea baseInfoOutPut;
 
+
+    /*
+    *   初始化 各个模块的按钮以及默认值
+    */
+    @FXML
+    void initialize() throws Exception {
+        this.initVulsChoise();  // 首页默认的漏洞编号
+    }
+
     private void initVulsChoise(){
         //有问题，没办法添加到选择框框中
-//        List<String> list= new ArrayList<>();
-//        list.add("cve-2022-1212");
-//        list.add("cve-2015-1234");
-//        vulsNameOfAll.setItems(FXCollections.observableArrayList(list));
+        List<String> list= new ArrayList<>();
+        list.add("cve-2022-1212");
+        list.add("cve-2015-1234");
+        list.add("ALL_ECHO");
+        vulsNameOfAll.setItems(FXCollections.observableArrayList(list));
     }
 
     @FXML
@@ -44,13 +54,20 @@ public class HelloController {
         String rmiUrl = rmiInputForCheck.getText().strip();
         String jrmpUrl = jrmpInputForCheck.getText().strip();
         String dnsLogURL = dnsLogInputForCheck.getText().strip();
+        String vulNameCheck = vulsNameOfAll.getValue();
 
         baseInfoOutPut.setText("[*] 解析单个URL检测时,设置的所有参数:\n");
         baseInfoOutPut.appendText("目标: " + targetUrl + "\n");
         baseInfoOutPut.appendText("JNDI: " + jndiUrl + "\n");
         baseInfoOutPut.appendText("RMI: " + rmiUrl + "\n");
         baseInfoOutPut.appendText("JRMP: " + jrmpUrl + "\n");
-        baseInfoOutPut.appendText(dnsLogURL + dnsLogURL + "\n");
+        baseInfoOutPut.appendText("DNSLog: " + dnsLogURL + "\n");
+        List<String> checkVulsName = new ArrayList<>();
+        if(vulNameCheck.contains("ALL_ECHO")){
+            checkVulsName.add();
+        }
+
+        baseInfoOutPut.appendText("VULName: " + vulNameCheck + "\n");
         baseInfoOutPut.appendText("[*] 参数解析完成，开始检测漏洞...");
 
     }
---
+++ src/main/java/com/example/weblogicexp/VulInfo/VulNames.java
@@ -0,0 +1,11 @@
+package com.example.weblogicexp.VulInfo;
+
+import java.util.List;
+
+public class VulNames {
+
+    public List<String> getAllVulNames(){
+
+
+    }
+}
