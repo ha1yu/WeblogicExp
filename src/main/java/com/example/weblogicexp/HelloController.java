3c4439c9e9ab4ecfb27be0df385f0d25f471ef6e
butnomingzi <admin@butnomingzi.com>
2023-04-22 16:08:55 +0800

    change UI

--- src/main/java/com/example/weblogicexp/HelloController.java
+++ src/main/java/com/example/weblogicexp/HelloController.java
@@ -1,14 +1,57 @@
 package com.example.weblogicexp;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcxxxxx");
+    private TextField urlInputForCheck;
+
+    @FXML
+    private TextField jndiInputForCheck;
+
+    @FXML
+    private TextField rmiInputForCheck;
+
+    @FXML
+    private TextField jrmpInputForCheck;
+    @FXML
+    private TextField dnsLogInputForCheck;
+    @FXML
+    private ComboBox<String> vulsNameOfAll;
+
+    @FXML
+    private TextArea baseInfoOutPut;
+
+    private void initVulsChoise(){
+        //有问题，没办法添加到选择框框中
+//        List<String> list= new ArrayList<>();
+//        list.add("cve-2022-1212");
+//        list.add("cve-2015-1234");
+//        vulsNameOfAll.setItems(FXCollections.observableArrayList(list));
+    }
+
+    @FXML
+    private void StartCheckAllVuls(){
+        // 获取URL vuls编号 JNDI RMI JRPM DNSlog， URL不可为空，其他可为空。
+        String targetUrl = urlInputForCheck.getText().strip();
+        String jndiUrl = jndiInputForCheck.getText().strip();
+        String rmiUrl = rmiInputForCheck.getText().strip();
+        String jrmpUrl = jrmpInputForCheck.getText().strip();
+        String dnsLogURL = dnsLogInputForCheck.getText().strip();
+
+        baseInfoOutPut.setText("[*] 解析单个URL检测时,设置的所有参数:\n");
+        baseInfoOutPut.appendText("目标: " + targetUrl + "\n");
+        baseInfoOutPut.appendText("JNDI: " + jndiUrl + "\n");
+        baseInfoOutPut.appendText("RMI: " + rmiUrl + "\n");
+        baseInfoOutPut.appendText("JRMP: " + jrmpUrl + "\n");
+        baseInfoOutPut.appendText(dnsLogURL + dnsLogURL + "\n");
+        baseInfoOutPut.appendText("[*] 参数解析完成，开始检测漏洞...");
+
     }
 }
